812735d3b61e19c4ec73f9766f448cbe229efad3
Alan Phelps <phelpsa64@gmail.com>
2022-04-13 18:21:30 -0400

    modal closes; modal component tested (it passed)

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-function Modal({ currentPhoto }) {
+const Modal = ({ onClose, currentPhoto }) => {
   const { name, category, description, index } = currentPhoto;
-
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
@@ -12,7 +11,7 @@ function Modal({ currentPhoto }) {
           alt="current category"
         />
         <p>{description}</p>
-        <button type="button">Close this modal</button>
+        <button onClick={onClose} type="button">Close this modal</button>
       </div>
     </div>
   );
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -125,14 +125,16 @@ const PhotoList = ({ category }) => {
 
   const toggleModal = (image, i) => {
     setCurrentPhoto({ ...image, index: i });
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   };
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}{" "}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
